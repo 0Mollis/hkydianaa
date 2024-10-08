67c203926c03d629a264b80ce25fd8d7b818a0a9
0Mollis <maks.chudnov.05@mail.ru>
2024-10-08 17:01:25 +0300

    helped

--- DB/index.php
+++ DB/index.php
@@ -22,44 +22,12 @@ $db_host = "localhost";
 $db_user = "root";
 $db_password = "root";
 $db_name = "lab";
-$connect = mysql_connect();
-
+$mysqli = new mysqli($db_host, $db_user, $db_password, $db_name);
 if ($mysqli->connect_error) {
     die('Ошибка: ('.$mysqli->connect_errno.')'.$mysqli->connect_error);
 }
-$selected = mysql_select_db($db_name, $connect);
-$query = "SELECT * FROM mytable";
-$result = mysql_query($sql);
-echo $sql;
-
-
-//INSERT INTO `mytable` (`id`, `name`, `l_name`, `age`, `phone`, `dolznost`, `zarpl`) VALUES
-
-
-
-
-
-/* 
-$name = $_POST['name'];
-$l_name = $_POST['l_name'];
-$age = $_POST['age'];
-$phone = $_POST['phone'];
-$dolznost = $_POST['dolznost'];
-$zarpl = $_POST['zarpl'];
-$db_table = "mytable";
-if ($mysqli->connect_error) {
-    die('Ошибка: ('.$mysqli->connect_errno.')'.$mysqli->connect_error);
-}
-$db_host = "localhost";
-$db_user = "root";
-$db_password = "root";
-$db_name = "lab";
-$link = mysql_connect($db_host,$db_user,$db_password);
-mysql_select_db($db_name, $link);
-$query = "INSERT INTO ".$db_table."(name,l_name,age,phone,dolznost,zarpl) VALUES 
-($name,$l_name,$age,$phone,$dolznost,$zarpl)";
-$result = $mysqli->query($query, $link);
-*/
+$result = $mysqli->query ("INSERT INTO ".$db_table."(name,l_name,age,phone,dolznost,zarpl) VALUES 
+($name,$l_name,$age,$phone,$dolznost,$zarpl)");
 ?>
 </body>
 </html>
