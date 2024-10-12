c6f15b85972d674115070d091ebb2c6dcc3ae820
0Mollis <maks.chudnov.05@mail.ru>
2024-10-12 21:22:50 +0300

    New Lab index.php

--- DB/index.php
+++ DB/index.php
@@ -8,36 +8,33 @@
 	</script>
 </head>
 <body>
-<form id="frm" method="POST">
-	<input type="text" name="name" placeholder="Имя">
-	<input type="text" name="l_name" placeholder="Фамилия">
-	<input type="text" name="age" placeholder="Возраст">
-	<input type="text" name="phone" placeholder="Телефон">
-	<input type="text" name="dolznost" placeholder="Должность">
-	<input type="text" name="zarpl" placeholder="Зарплата">
-	<input type="submit" value="Сохранить" onclick="view()">
-	<b>
-</form>
 <?php
-$name = $_POST["name"];
+/*$name = $_POST["name"];
 $l_name = $_POST["l_name"];
 $age = $_POST["age"];
 $phone = $_POST["phone"];
 $dolznost = $_POST["dolznost"];
-$zarpl = $_POST["zarpl"];
+$zarpl = $_POST["zarpl"];*/
 
 $db_host = "localhost";
 $db_user = "root";
 $db_password = "root";
 $db_name = "lab";
-$my_tab = "mytable";
+$my_tab = "lab2";
 
 $mysqli = new mysqli($db_host, $db_user, $db_password, $db_name);
 if ($mysqli->connect_error) {
     die('Ошибка: ('.$mysqli->connect_errno.')'.$mysqli->connect_error);
 }
-$mysqli->query ("INSERT INTO ".$my_tab." (name,l_name,age,phone,dolznost,zarpl) VALUES 
-('$name','$l_name','$age','$phone','$dolznost','$zarpl')");
+
+$result = $mysqli->query ("SELECT `text` FROM lab2");
+echo($result);
+/*while($row = mysqli_fetch_assoc($result)){
+	echo htmlspecialchars_decode(stripslashes($row[0]));
+}*/
+
+/*$mysqli->query ("INSERT INTO ".$my_tab." (name,l_name,age,phone,dolznost,zarpl) VALUES 
+('$name','$l_name','$age','$phone','$dolznost','$zarpl')");*/
 
 
 ?>
