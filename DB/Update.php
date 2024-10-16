e6b331ecd905aa067b630cc1fbc25280007dd9ff
0Mollis <maks.chudnov.05@mail.ru>
2024-10-16 12:39:44 +0300

    новая штука

--- DB/Update.php
+++ DB/Update.php
@@ -1,11 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>БД</title>
-	<script type="text/javascript">
-	</script>
+	<title>изменение</title>
 </head>
 <body>
 <?php
@@ -14,6 +10,7 @@ $db_user = "root";
 $db_password = "root";
 $db_name = "lab";
 $my_tab = "lab2";
+$tab_id = 2;
 
 $link = new mysqli($db_host, $db_user, $db_password, $db_name);
 if ($mysqli->connect_error) {
@@ -22,16 +19,11 @@ if ($mysqli->connect_error) {
 $query = "SELECT `text` FROM lab2 WHERE id = 2";
 $result = mysqli_query($link,$query);
 while ($rows = mysqli_fetch_array($result)) {
-	echo($rows[0]);
+	echo('<form method="POST"><textarea name="upd">"'.$rows[0].'"</textarea><br><input type="submit" name="but"></form>');
 }
-
-
-
-
-/*$mysqli->query ("INSERT INTO ".$my_tab." (name,l_name,age,phone,dolznost,zarpl) VALUES 
-('$name','$l_name','$age','$phone','$dolznost','$zarpl')");*/
-
-
+$forUpdate = $_POST["upd"];
+$query = "UPDATE `lab2` SET `text` ='".$forUpdate."'WHERE `id` ='".$tab_id."';";
+$result = mysqli_query($link,$query);
 ?>
 </body>
 </html>
