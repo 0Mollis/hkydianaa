ff32428827a9744198205ac9681b1bb85bb156cd
0Mollis <maks.chudnov.05@mail.ru>
2024-10-19 10:47:20 +0300

    work

---
+++ Update.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>изменение</title>
+</head>
+<body>
+<?php
+$db_host = "localhost";
+$db_user = "root";
+$db_password = "root";
+$db_name = "lab";
+$my_tab = "lab2";
+$tab_id = 2;
+
+$link = new mysqli($db_host, $db_user, $db_password, $db_name);
+if ($mysqli->connect_error) {
+    die('Ошибка: ('.$mysqli->connect_errno.')'.$mysqli->connect_error);
+}
+$query = "SELECT `text` FROM lab2 WHERE id = 2";
+$result = mysqli_query($link,$query);
+while ($rows = mysqli_fetch_array($result)) {
+	echo('<form method="POST"><textarea name="upd">'.$rows['text'].'</textarea><br><input type="submit" name="but"></form>');
+}
+$forUpdate = $_POST["upd"];
+$query = "UPDATE `lab2` SET `text` ='".$forUpdate."'WHERE `id` ='".$tab_id."';";
+$result = mysqli_query($link,$query);
+?>
+</body>
+</html>
